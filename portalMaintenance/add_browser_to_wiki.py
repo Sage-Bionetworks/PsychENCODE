9b643a648d34784228a0c83ad1afb0e0321ff6fb
kkdang <kristen.dang@sagebase.org>
2016-01-26 09:50:19 -0800

    Fixes check for mouse.

--- portalMaintenance/add_browser_to_wiki.py
+++ portalMaintenance/add_browser_to_wiki.py
@@ -18,7 +18,7 @@ for result in results:
 		if result['file.organism'][0] == "Homo sapiens":
 			browser = ''.join([' """${biodalliance13?chr=1&species=HUMAN&viewStart=3025001&viewEnd=3525001&source0=%7B"name"%3A""%2C "entityId"%3A"', result['file.id'], '"%2C "entityVersion"%3A"', str(result['file.versionNumber']), '"%2C "styleType"%3A"default"%2C "styleGlyphType"%3A"HISTOGRAM"%2C "color"%3A"%23808080"%2C "type"%3A"BIGWIG"%2C "height"%3A"120"%7D}""" ' ])
 #			print '%s' % browser
-		elif result['file.organism'][0] is "Mus musculus":
+		elif result['file.organism'][0] == "Mus musculus":
 			browser = ''.join([' """${biodalliance13?chr=1&species=MOUSE&viewStart=3025001&viewEnd=3525001&source0=%7B"name"%3A""%2C "entityId"%3A"', result['file.id'], '"%2C "entityVersion"%3A"', str(result['file.versionNumber']), '"%2C "styleType"%3A"default"%2C "styleGlyphType"%3A"HISTOGRAM"%2C "color"%3A"%23808080"%2C "type"%3A"BIGWIG"%2C "height"%3A"120"%7D}""" ' ])		
 		wiki = Wiki(owner=temp, markdown=browser)
 		wiki = syn.store(wiki)
