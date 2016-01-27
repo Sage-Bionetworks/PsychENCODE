f213b57d481fe9633f30f8cadc3f26c530b3821f
kkdang <kristen.dang@sagebase.org>
2016-01-27 14:23:12 -0800

    Gets YAML from synapse, excludes ubiquitous platform terms, properly counts unique values.

--- portalMaintenance/check_annotations_against_dictionary.py
+++ portalMaintenance/check_annotations_against_dictionary.py
@@ -2,28 +2,37 @@
 # KKD for Sage Bionetworks
 # Jan. 25, 2016
 
-# sys.argv[1] = yaml file of annotation dictionary
+# sys.argv[1] = synapse ID of (yaml) annotation dictionary
 # sys.argv[2] = project ID to check
 
 import sys, yaml
 import synapseclient
 
 syn = synapseclient.login()
+yamlEnt = syn.get(sys.argv[1])
 
-annotations = yaml.load(file(sys.argv[1]))
+annotations = yaml.load(file(yamlEnt.path))
 
 allKeysInProject = set()
 allValsInProject = set()
 
+systemKeysToExclude = ['creationDate', 'etag', 'id', 'uri', 'accessControl']
+
+#results = syn.chunkedQuery('select id from file where projectId=="%s" and file.assay=="ChIP-seq"' % sys.argv[2])
 results = syn.chunkedQuery('select id from file where projectId=="%s"' % sys.argv[2])
 for result in results:
 	temp = syn.getAnnotations(result['file.id'])
 	for key in temp:
-		if key in allKeysInProject: continue
-		allKeysInProject.add(key)
-		for val in temp[key]:
-			if val in allValsInProject: continue
-			allValsInProject.add(val)
+		if key in systemKeysToExclude: continue
+		if not key in allKeysInProject:
+			allKeysInProject.add(key)
+		if isinstance(temp[key], list):
+			for val in temp[key]:
+				if val in allValsInProject: continue
+				allValsInProject.add(val)
+		else:
+			if temp[key] in allValsInProject: continue
+			allValsInProject.add(temp[key])
 
 print 'Number of key terms in project: %d' % len(allKeysInProject)
 print 'Number of value terms in project: %d' % len(allValsInProject)
