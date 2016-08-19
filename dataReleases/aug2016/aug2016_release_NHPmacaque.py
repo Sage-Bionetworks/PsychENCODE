e3960945ac95e0af0ddc0561125d977fd0695923
Kristen Dang <kristen.dang@gmail.com>
2016-08-19 10:53:52 -0700

    nhp-macaque near final.

--- dataReleases/aug2016/aug2016_release_NHPmacaque.py
+++ dataReleases/aug2016/aug2016_release_NHPmacaque.py
@@ -9,49 +9,66 @@ import pythonSynapseUtils as synAnnot
 from pythonSynapseUtils import auditAndUpdateByCommonDict
 from pythonSynapseUtils import auditAndUpdateFormatTypeByFileName
 from pythonSynapseUtils import auditAndUpdateByMetadata
-
+from pythonSynapseUtils import delAnnoByKey
+from pythonSynapseUtils import annotationsYaml
 
 # NHP-Macaque
 #syn7065089
 
+# synAnnot.delAnnoByKey.delAnnoByKey(syn,"syn7065089",["tissueType","readLength"])
+# synAnnot.delAnnoByKey.delAnnoByKey(syn,"syn7065089",["platform", "tissueType"])
 
-# Audit and update by common annotations
-commonDict = {"consortium":"PEC",
-              "grant":"U01MH103339",
-              "group":"Yale, UCSF",
-              "assay":"RNA-seq",
-              "libraryPrep":"polyA selection",
-              "runType":"single-end",
-              "study":"NHP	",
-              "disease":"Control",
-              "fileType":"bam",
-              "PI":"Nenad Sestan, Matt State",
-              "organism":"Rhesus macaque"
-             }
-
-#synAnnot.auditAndUpdateByCommonDict.updateAnnoByDict(syn,"syn7065089",commonDict)
-
-
-#entityMissAllAnno, incorrectAnnoated, missingAnno = synAnnot.auditAndUpdateByCommonDict.auditCommonDict(syn,"syn7065089",commonDict)
-
-
+# 
+# # Audit and update by common annotations
+# commonDict = {"consortium":"PEC",
+#               "grant":"U01MH103339",
+#               "group":"Yale, UCSF",
+#               "assay":"RNA-seq",
+#               "libraryPrep":"polyA selection",
+#               "runType":"single-end",
+#               "study":"NHP	",
+#               "disease":"Control",
+#               "fileType":"bam",
+#               "PI":"Nenad Sestan, Matt State",
+#               "organism":"Rhesus macaque",
+#               "tissueType":"",
+#               "platform":"GAIIx",
+#               "readLength":""
+#              }
+# 
+# synAnnot.auditAndUpdateByCommonDict.updateAnnoByDict(syn,"syn7065089",commonDict)
+# 
+# 
+# entityMissAllAnno, incorrectAnnoated, missingAnno = synAnnot.auditAndUpdateByCommonDict.auditCommonDict(syn,"syn7065089",commonDict)
+# 
+# 
 # result = synAnnot.auditAndUpdateFormatTypeByFileName.auditFormatTypeByFileName(syn,"syn7065089","fileType",{".bam":"bam"})
 # for key in result:
 # 	print '%s: %d' % (key, len(result[key]))
-# 
+
 
 # Update by metadata
 rnaSeqData = syn.get("syn7105855")
 rnaSeqData = pd.read_table(rnaSeqData.path, sep = ",")
 
-df = rnaSeqData[["File_Name","BrainRegion Description"]]
-df.columns = ["fileName","tissueType"]
+df = rnaSeqData[["File_Name","BrainRegion Description","ReadLength", "SequencingPlatform"]]
+df.columns = ["fileName","tissueType","readLength","platform"]
 
-entityMissMetadata,incorrectAnnotated, missingAnno = synAnnot.auditAndUpdateByMetadata.auditAgainstMetadata(syn,"syn7065089",df,"fileName",["BrainRegion Description"],[".bam"])
+# entityMissMetadata,incorrectAnnotated, missingAnno = synAnnot.auditAndUpdateByMetadata.auditAgainstMetadata(syn,"syn7065089",df,"fileName",["tissueType","readLength"],[""])
+# 
+# print 'entityMissMetadata: %d' % len(entityMissMetadata)
+# print 'incorrectAnnotated: %d' % len(incorrectAnnotated)
+# print 'missingAnno: %d' % len(missingAnno)
+# 
+# if len(entityMissMetadata) > 0:
+# 	for key in entityMissMetadata:
+# 		print key
+#  
+# synAnnot.auditAndUpdateByMetadata.updateAnnoByMetadata(syn,"syn7065089",df,"fileName",["tissueType","readLength"],[""])
 
-print 'entityMissMetadata: %d' % len(entityMissMetadata)
-print 'incorrectAnnotated: %d' % len(incorrectAnnotated)
-print 'missingAnno: %d' % len(missingAnno)
- 
-# Bug in code here...global variable conflict/undefined? 
-#synAnnot.auditAndUpdateByMetadata.updateAnnoByMetadata(syn,"syn7065089",df,"fileName",["tissueType"],[".bam"])
+synAnnot.auditAndUpdateByMetadata.updateAnnoByMetadata(syn,"syn7065089",df,"fileName",["platform", "tissueType"],[""])
+
+
+(wrongKeys, wrongValues) = synAnnot.annotationsYaml.checkAgainstDict(syn=syn, synId="syn7065089",annotDictId="syn5605845")
+
+
