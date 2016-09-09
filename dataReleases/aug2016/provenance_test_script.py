53441a48019be9f625b146b0d31930e4f4fa5844
Kennedy Agwamba <kagwamba@w89274sa66h.fhcrc.org>
2016-09-09 14:43:50 -0700

    Modified script to accept parentId as input along with minor bug fixes

--- dataReleases/aug2016/provenance_test_script.py
+++ dataReleases/aug2016/provenance_test_script.py
@@ -4,55 +4,58 @@
 #an argument. 
 
 import argparse
-import logging
 import json
 import pandas as pd
 from py2neo import Graph, authenticate
 
 # Build query
 query = """
-MATCH (a {{fileType:"{0}"}})-[r]-(s)-[t]->(fq {{fileType:"{1}"}}) WHERE a.projectId="{2}"
-   RETURN a.name, collect(distinct fq.name), count(distinct fq.name) LIMIT 5
+MATCH (a {{fileType:"{0}", parentId:"{1}"}})-[r]-(s)-[t]->(fq {{fileType:"{2}"}}) WHERE a.projectId="{3}"
+   RETURN a.name, collect(distinct fq.name), count(distinct fq.name)
 """
 
-def queryGraphDB(filename, filetypeA, filetypeB, projectId):
-    global graph, query
-    query = query.format(filetypeA, filetypeB, projectId)
+def queryGraphDB(filename, filetypeA, parentId, filetypeB, projectId, graph):
+    global query
+    query = query.format(filetypeA, parentId, filetypeB, projectId)
     print 'Printing formatted query'
     print query
+    print 'Sending query'
 
     # Send Cypher query
-    logging.info('Sending query')
     df = pd.DataFrame(graph.run(query).data())
     df.to_csv(str(filename))
-
-    logging.info('Done.')
+    print 'Done.'
 
 if __name__ == '__main__':
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
     parser = argparse.ArgumentParser(description=
                 'Please input the projectId to generate a csv relating to provenance')
     parser.add_argument('file', nargs='?', help='Input the filename to write results to')
     parser.add_argument('--fileType1', nargs='?', default='bam', help='Input the main file type for which provenance is being checked without period e.g. bam')
+    parser.add_argument('--parent', nargs='?', default='syn7105852', help='Input parentId identifying folder of first file type')
     parser.add_argument('--fileType2', nargs='?', default='fastq', help='Input the secondary file type for which provenance is being checked against without period e.g. fastq')
     parser.add_argument('--project', nargs='?', help='Input the projectId in the following format #######.0: Default is PsychENCODE')
     args = parser.parse_args()
 
     # Connect to graph
-    logging.info('Connecting to Neo4j and authenticating user credentials')
+    print 'Connecting to Neo4j and authenticating user credentials'
     with open('credentials.json') as json_file:
         db_info=json.load(json_file)
     authenticate(db_info['machine'], db_info['username'], db_info['password'])
     db_dir = db_info['machine'] + "/db/data"
+    print db_dir
     graph = Graph(db_dir) #can remove db_dir if localhost
 
     filename = args.file
     filetypeA = args.fileType1
+    parentId = args.parent
     filetypeB = args.fileType2
     if args.project:
         projId = args.project
     else:
         projId = str(4921369.0)
-
-    queryGraphDB(filename, filetypeA, filetypeB, projId)
+    
+    try:
+        queryGraphDB(filename, filetypeA, parentId, filetypeB, projId, graph)
+    except:
+        print 'Error involving transaction with Neo4j database'
+        raise
